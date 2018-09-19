daf2c894eb29226165dbfa62ccd1ee1de0a728bb
Peter King <73837king@gmail.com>
2018-09-19 18:40:21 -0400

    typing project name in all three cards  done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 
-
+  /////////////////////////////////////////////////////////////////////////////
+  // scroll logic - BEGIN
+  /////////////////////////////////////////////////////////////////////////////
   const scrollTo = (e) => {
     e.preventDefault();
     const target = e.currentTarget.hash;
@@ -9,47 +11,18 @@ $(function() {
     }, 1500,'swing');
     return false;
   };
+  /////////////////////////////////////////////////////////////////////////////
+  // scroll logic - END
+  /////////////////////////////////////////////////////////////////////////////
 
-  // // dynamic sticky nav - BEGIN
-  // const navOffset = $('.nav').offset();
-  // const navOffsetTop = navOffset.top;
-  // window.onscroll = (e) => {stickyNav(e)};
-  //
-  // const stickyNav = (e) => {
-  //   console.log('stickyNav ran');
-  //   console.log('window.pageYOffset', window.pageYOffset);
-  //   console.log('navOffsetTop', navOffsetTop);
-  //   if (window.pageYOffset > navOffsetTop) {
-  //     $('.nav').addClass("sticky");
-  //     $('.nav a').addClass("a-text");
-  //   } else {
-  //     $('.nav').removeClass("sticky");
-  //     $('.nav a').removeClass("a-text");
-  //   }
-  // };
-  // // dynamic sticky nav - END
+  /////////////////////////////////////////////////////////////////////////////
+  // typing logic - BEGIN
+  /////////////////////////////////////////////////////////////////////////////
+  let isTypingCard1 = false;
+  let isTypingCard2 = false;
+  let isTypingCard3 = false;
 
-
-  // function typeText(item, text, delay, i) {
-  //   $(item).append(text.charAt(i))
-  //          .delay(delay)
-  //          .promise()
-  //          .done(function() {
-  //            if(i<text.length) {
-  //              i++;
-  //              typeText(item, text, delay, i);
-  //            }
-  //          });
-  // }
-  //
-  // var text = 'Some Text';
-  // var item = $("div");
-
-  let isTyping = false;
-  const type = (textSpan, text, delay, i) => {
-    console.log('type ran');
-
-    console.log('span html =', $(textSpan).html());
+  const type = (textSpan, text, delay, i, cardId) => {
 
     $(textSpan)
     .append(text.charAt(i))
@@ -58,67 +31,71 @@ $(function() {
     .done(() => {
       if(i < text.length) {
         i++;
-        isTyping = true;
-        type(textSpan, text, delay, i)
+        if (cardId === 'card-1') { isTypingCard1 = true; }
+        if (cardId === 'card-2') { isTypingCard2 = true; }
+        if (cardId === 'card-3') { isTypingCard3 = true; }
+        type(textSpan, text, delay, i, cardId)
       } else {
-        console.log('done typing');
-        isTyping = false;
+        // done typing.
+        if (cardId === 'card-1') { isTypingCard1 = false; }
+        if (cardId === 'card-2') { isTypingCard2 = false; }
+        if (cardId === 'card-3') { isTypingCard3 = false; }
       }
     });
 
   };
 
   const typeText = (e) => {
-    console.log('typeText ran');
     const $card = $(e.currentTarget);
     const cardId = $card.attr('id');
-    const cardText1 = 'Air Bites n Bars';
-    const cardText2 = 'somm assistance';
-    const cardText3 = 'Diners, Drive-ins & Dives Tracker';
+
     let text = '';
     let textSpan = '';
-    let i = 0;
+    const i = 0;
 
-    if (cardId === 'card-1') {
-      text = cardText1;
-      textSpan = $('.text-span-1')[0];
-
-    }
-
-    const $textSpan = $(textSpan);
-    console.log('$card', $card);
-    console.log('cardId', cardId);
-    console.log('textSpan', textSpan);
-    console.log('textSpan.html()', $(textSpan).html());
-
-    // if ($textSpan.html()) {
-    if (isTyping) {
-      // in case User mousenters again before text() has completed.
-      // console.log('if $textSpan.html()', $textSpan.html());
-      console.log('if isTyping ');
+    // Card 1
+    if (cardId === 'card-1' && isTypingCard1 ) {
+      // Handles when User Enters, Leaves,
+      // then Re-Enters Card Overlay before typing has finished.
       return;
-    } else {
-      console.log('in else');
-      $textSpan.empty();
-      type(textSpan, text, 500, i);
+    }
+    if ( cardId === 'card-1') {
+      text = 'Air Bites n Bars';
+      textSpan = $('.text-span-1')[0];
     }
 
-    // $textSpan.empty();
-    // console.log('$textSpan ', $textSpan);
-    // type(textSpan, text, 500, i);
+    // Card 2
+    if (cardId === 'card-2' && isTypingCard2 ) {
+      // Handles when User Enters, Leaves,
+      // then Re-Enters Card Overlay before typing has finished.
+      return;
+    }
+    if ( cardId === 'card-2') {
+      text = 'somm assistance';
+      textSpan = $('.text-span-2')[0];
+    }
+
+    // Card 3
+    if (cardId === 'card-3' && isTypingCard3 ) {
+      // Handles when User Enters, Leaves,
+      // then Re-Enters Card Overlay before typing has finished.
+      return;
+    }
+    if ( cardId === 'card-3') {
+      text = 'Diners, Drive-ins & Dives Tracker';
+      textSpan = $('.text-span-3')[0];
+    }
+
+    $(textSpan).empty();
+    type(textSpan, text, 300, i, cardId);
 
   };
-
-  // const cardMouseleave = (e) => {
-  //   const $card = $(e.currentTarget);
-  //   const cardId = $card.attr('id');
-  //   const textSpan = $('.text-span-1')[0];
-  //   $(textSpan).empty();
-  // };
+  /////////////////////////////////////////////////////////////////////////////
+  // typing logic - END
+  /////////////////////////////////////////////////////////////////////////////
 
   // Handlers
   $('.nav-link a').on('click', (e) => {scrollTo(e)});
   $('.card').on('mouseenter', (e) => {typeText(e)});
-  $('.card').on('mouseleave', (e) => {cardMouseleave(e)});
 
 });
